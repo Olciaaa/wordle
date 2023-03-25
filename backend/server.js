83ab3516ac22d5da6bdebe8443b5a62074b1f3e5
poskrobe <ola.poskrobek@gmail.com>
2023-03-25 16:56:11 +0100

    server checked

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require('cors')
 const bodyParser = require('body-parser');
+const port = 4000;
 
 let app = express()
 app.use(cors())
@@ -8,19 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 require('dotenv').config();
 
-const sql = require('./db.js');
-
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
     res.send({ status: "OK" })
 })
 
-const authRoute = require('./routes/auth')
-app.use("/auth", authRoute)
-
-const userRoute = require('./routes/user')
-app.use("/user", userRoute)
-
-
-app.listen(process.env.PORT, function () {
-    console.log("Backend API listening on port " + process.env.PORT)
+app.listen(port, function () {
+    console.log("Backend API listening on port " + port)
 })
