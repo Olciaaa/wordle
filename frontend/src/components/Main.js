811e220040fbb0e635447c8c039fc63973c3406b
poskrobe <ola.poskrobek@gmail.com>
2023-03-25 18:46:59 +0100

    composition

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Header';
 import '../styles/main.css';
 
 const Main = () => {
     return (
         <div className="main">
-            Main component
+            <hr></hr>
+            <Header cssStyle={{'height': 200}}></Header>
+            <hr></hr>
         </div>
     );
 };
